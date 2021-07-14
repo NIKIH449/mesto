c6e5f46e9fb635462c0f94be229d4a5dece2b8d2
Nikih449 <druzhevskii@gmail.com>
2021-07-14 16:11:47 +0300

    feat: попап теперь сохраняет

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 let main = document.querySelector('.main');
 let openPopup = main.querySelector('.popup_opened');
 let editButton = main.querySelector('.profile__button-edit');
-let formPopup = main.querySelector('.popup_opened');
 let closeButton = main.querySelector('.popup__button-close');
+let saveButton = main.querySelector('.popup__button-save');
+
 
 function showPopup () {
-  formPopup.classList.remove('popup_opened');
+
+  openPopup.classList.remove('popup_opened');
 }
 editButton.addEventListener('click', showPopup);
 
 function closePopup () {
-  formPopup.classList.add('popup_opened');
+  openPopup.classList.add('popup_opened');
 }
 closeButton.addEventListener('click', closePopup);
+
+
+
+
+function formSubmitHandler (evt) {
+
+  let formElement = main.querySelector('.popup__container');
+  let nameInput = formElement.querySelector('.popup__name');
+  let jobInput = formElement.querySelector('.popup__desription');
+  let profileName = main.querySelector('.profile__name');
+  let profileDiscription = main.querySelector('.profile__discription');
+
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDiscription.textContent = jobInput.value;
+
+  nameInput.textContent = profileName;
+  jobInput.textContent = profileDiscription;
+
+  closePopup()
+  }
+
+saveButton.addEventListener('click', formSubmitHandler);
