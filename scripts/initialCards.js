abf01767172b7d9e7df1a3b9f4526297e8af7b8b
Nikih449 <druzhevskii@gmail.com>
2021-09-17 11:23:43 +0300

    refator: сменить картинки на онлайновые

--- scripts/initialCards.js
+++ scripts/initialCards.js
@@ -2,32 +2,32 @@
 const initialCards = [
   {
     name: 'Сент-Джордж',
-    link: './images/centjeorge.jpg',
+    link: 'https://images.unsplash.com/photo-1579700286429-6ca39e12e4cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
     alt: 'Сент-Джордж',
   },
   {
     name: 'Иерусалим',
-    link: './images/jerusalem.jpg',
+    link: 'https://images.unsplash.com/photo-1464980195772-d5d6e9435fac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
     alt: 'Иерусалим',
   },
   {
-    name: 'Мёртвое море',
-    link: './images/deadsea.jpg',
-    alt: 'Мёртвое море',
+    name: 'Мёрвтое море',
+    link: 'https://images.unsplash.com/photo-1601668246782-863c9c89a095?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+    alt: 'Мёрвтое море',
   },
   {
     name: 'Яффо',
-    link: './images/yaffo.jpg',
+    link: 'https://images.unsplash.com/photo-1518728242875-50600dfbe31a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
     alt: 'Яффо',
   },
   {
     name: 'Масада',
-    link: './images/masada.jpg',
+    link: 'https://images.unsplash.com/photo-1592989847276-d9d6a8dab73d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
     alt: 'Масада',
   },
   {
     name: 'Тель-Авив',
-    link: './images/telaviv.jpg',
+    link: 'https://images.unsplash.com/photo-1577552919665-554b5dc35993?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80',
     alt: 'Тель-Авив',
   },
 ];
